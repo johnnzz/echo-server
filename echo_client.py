9fb8deaabd12f77030ea0494755ddda6d321ae79
John Navitsky <johnn@cray.com>
2018-04-16 22:15:00 -0700

    Updated echo_server and echo_client to pass unit tests and complete TODO items.

--- echo_client.py
+++ echo_client.py
@@ -3,39 +3,59 @@ import sys
 
 
 def client(msg, log_buffer=sys.stderr):
-    server_address = ('localhost', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
-    # TODO: connect your socket to the server here.
 
-    # you can use this variable to accumulate the entire message received back
-    # from the server
+    # create a socket instance
+    server_address = ('localhost', 10000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect to the server address/port
+    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
+    sock.connect(server_address)
+
+    # accumulate the entire message received back from the server
     received_message = ''
 
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
+
         print('sending "{0}"'.format(msg), file=log_buffer)
-        # TODO: send your message to the server here.
 
-        # TODO: the server should be sending you back your message as a series
-        #       of 16-byte chunks. Accumulate the chunks you get to build the
-        #       entire reply from the server. Make sure that you have received
-        #       the entire message and then you can break the loop.
-        #
-        #       Log each chunk you receive.  Use the print statement below to
-        #       do it. This will help in debugging problems
-        chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+        # loop over the sending string by 16 byte chunks
+        for i in range(0, len(msg), 16):
+
+            # split out the bit we want to send
+            send_chunk = msg[i:i+16]
+
+            # convert it to bytes
+            b = bytes(send_chunk, 'utf-8')
+
+            # send it to the server
+            sock.send(b)
+
+            # the server should be sending you back the message as a series
+            # of 16-byte chunks. We accumulate the chunks to build the
+            # entire reply from the server.
+
+            # get the response back from the server
+            chunk = sock.recv(16)
+            print('  chunk received: "{}"'.format(chunk.decode('utf-8')), file=log_buffer)
+
+            # build up the recived_message string
+            received_message += chunk.decode('utf-8')
+
     finally:
-        # TODO: after you break out of the loop receiving echoed chunks from
-        #       the server you will want to close your client socket.
+        # after we break out of the loop receiving echoed chunks from
+        # the server, close the client socket.
         print('closing socket', file=log_buffer)
+        sock.shutdown(socket.SHUT_RD)
+        sock.close()
 
-        # TODO: when all is said and done, you should return the entire reply
-        # you received from the server as the return value of this function.
+        # print the entire, echoed message
+        print('got back: "{}"'.format(received_message), file=log_buffer)
+
+        # return the entire reply
+        return received_message
 
 
 if __name__ == '__main__':
